a5528cfbe929e524ca2ea832c0b241327cf56b71
Michael Mansfield <m7mansfield@gmail.com>
2017-01-14 16:24:00 -0500

    Working and new deps

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const Tail = require('tail').Tail;
-const http = require('http');
+const request = require('request');
 const config = require('./config.json');
 
 tail = new Tail(config.logFile);
 tail.on("line", function(data) {
     var options = {
-        url: config.url,
-        method: 'POST',
-        body: data
+        text: data,
     };
-    http.request(options, function(error, response, body) {
+    request.post(config.url, {
+        body: JSON.stringify(options)
+    }, function(error, response, body) {
         if (error) console.log(error);
     });
 });
