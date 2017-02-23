7074b2583e20384dcb4759d4b40562e1d9f35ea0
Michael Mansfield <m7mansfield@gmail.com>
2017-02-23 17:41:44 -0500

    Limit requests to ~4000 char chunks to reduce spamming

--- index.js
+++ index.js
@@ -2,18 +2,25 @@ const Tail = require('tail').Tail;
 const request = require('request');
 const config = require('./config.json');
 
+var body = '';
+
 tail = new Tail(config.logFile);
-tail.on("line", function(data) {
-    var options = {
-        text: data,
-    };
-    request.post(config.url, {
-        body: JSON.stringify(options)
-    }, function(error, response, body) {
-        if (error) console.log(error);
-    });
+
+tail.on('line', function(data) {
+    if((body.length + data.length) > 3980) {
+        var options = {
+            text: body,
+        };
+        request.post(config.url, {
+            body: JSON.stringify(options)
+        }, function(error, response, body) {
+            if (error) console.log(error);
+        });
+        body = '';
+    }
+    body += data;
 });
 
 tail.on('error', (data)=> {
-    console.log("error:", data);
+    console.log('error:', data);
 });
